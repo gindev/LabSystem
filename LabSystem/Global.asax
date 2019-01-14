8dc3cf21c99354773a57dd8d024b47b89a85ba03
Panayot Gindev <gindev@live.com>
2019-01-14 09:50:05 +0200

    Add project files.

---
+++ LabSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabSystem.MvcApplication" Language="C#" %>
---
+++ LabSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabSystem.Startup))]
+namespace LabSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
